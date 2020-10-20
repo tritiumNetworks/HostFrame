4adee80a309551f496ca30a8ae93b998fdc53a7d
pmh-only <pmhstudio.pmh@gmail.com>
2020-10-20 22:52:50 +0900

    remove jupyter

--- router/jupyter/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = { _root: '/jupyter', _socket: false, _cors: false, _parser: [], ready }
-
-/**
- * @param {import('../../class/Rapp')} app
- */
-function ready (app) {
-  app.get('/', (req, res) => { res.redirect('https://' + req.hostname + ':2053') })
-}
